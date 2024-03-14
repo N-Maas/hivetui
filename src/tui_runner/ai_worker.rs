b6c4530c403de11b084258d487c3c0818988ca4e
Nikolai Maas <nikolai.maas@kit.edu>
2024-03-14 21:24:18 +0100

    ai worker restructuring

--- src/tui_runner/ai_worker.rs
+++ src/tui_runner/ai_worker.rs
@@ -14,58 +14,57 @@ use super::{
 use crate::ai::{Difficulty, HiveAI};
 
 pub fn start_ai_worker_thread(exchange_point: Arc<Mutex<AIExchange>>) {
-    let ais = [
-        HiveAI::new(Difficulty::Easy),
-        HiveAI::new(Difficulty::QuiteEasy),
-        HiveAI::new(Difficulty::Medium),
-        HiveAI::new(Difficulty::Hard),
-    ];
-    thread::spawn(move || loop {
+    thread::spawn(move || {
         setup_panic_reporting();
 
         let result = catch_unwind(|| {
-            thread::sleep(Duration::from_millis(10));
+            let ais = [
+                HiveAI::new(Difficulty::Easy),
+                HiveAI::new(Difficulty::QuiteEasy),
+                HiveAI::new(Difficulty::Medium),
+                HiveAI::new(Difficulty::Hard),
+            ];
+            loop {
+                thread::sleep(Duration::from_millis(10));
 
-            let Some(AIMessageForWorker::Start(level, state)) = ({
-                // careful: we need this construction to not accidentially prolong the life of the lock
+                let Some(AIMessageForWorker::Start(level, state)) = ({
+                    // careful: we need this construction to not accidentially prolong the life of the lock
+                    let mut exchange = exchange_point.lock().unwrap();
+                    exchange.for_worker.take()
+                }) else {
+                    continue;
+                };
+
+                let player = state.player();
+                let ai = &ais[usize::from(u8::from(level))];
+                let engine = Engine::new(2, *state);
+                let Some(mut ratings) = ai.run_all_ratings(&engine, || {
+                    exchange_point.lock().unwrap().for_worker.is_some()
+                }) else {
+                    continue;
+                };
+                ratings.sort_by_key(|(r, _, _)| -r);
+                let (_, best_move, _) = ratings.first().unwrap().clone();
+
                 let mut exchange = exchange_point.lock().unwrap();
-                exchange.for_worker.take()
-            }) else {
-                return;
-            };
-
-            let player = state.player();
-            let ai = &ais[usize::from(u8::from(level))];
-            let engine = Engine::new(2, *state);
-            let Some(mut ratings) = ai.run_all_ratings(&engine, || {
-                exchange_point.lock().unwrap().for_worker.is_some()
-            }) else {
-                return;
-            };
-            ratings.sort_by_key(|(r, _, _)| -r);
-            let (_, best_move, _) = ratings.first().unwrap().clone();
-
-            let mut exchange = exchange_point.lock().unwrap();
-            let return_result = match exchange.for_worker.as_ref() {
-                Some(AIMessageForWorker::Cancel | AIMessageForWorker::Start(_, _)) => false,
-                None => true,
-            };
-            if return_result {
-                exchange.for_runner = Some(AIMessageForRunner::Result(Box::new(AIResult {
-                    player,
-                    best_move: best_move,
-                    all_ratings: ratings,
-                    annotations: Default::default(),
-                })));
+                let return_result = match exchange.for_worker.as_ref() {
+                    Some(AIMessageForWorker::Cancel | AIMessageForWorker::Start(_, _)) => false,
+                    None => true,
+                };
+                if return_result {
+                    exchange.for_runner = Some(AIMessageForRunner::Result(Box::new(AIResult {
+                        player,
+                        best_move: best_move,
+                        all_ratings: ratings,
+                        annotations: Default::default(),
+                    })));
+                }
             }
         });
-        match result {
-            Ok(val) => val, // just propagate I/O error
-            Err(_) => {
-                let (msg, trace) = get_panic_data();
-                exchange_point.lock().unwrap().for_runner =
-                    Some(AIMessageForRunner::Killed(msg, trace));
-            }
+        if result.is_err() {
+            let (msg, trace) = get_panic_data();
+            exchange_point.lock().unwrap().for_runner =
+                Some(AIMessageForRunner::Killed(msg, trace));
         }
     });
 }
