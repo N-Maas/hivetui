964cbb3798fa75493f7b9c49930c92ee20bfa501
Nikolai Maas <nikolai.maas@kit.edu>
2024-10-12 23:41:36 +0200

    fix build script

--- build_targets.sh
+++ build_targets.sh
@@ -11,8 +11,9 @@ BuildBinary() {
         echo "build failed: $2" 1>&2
         exit 1
     fi
+    mkdir -p $target_folder/$2_tmp
     cp ./target/$1/release/hive $target_folder/$2_tmp
-    zip $target_folder/$2 $target_folder/$2_tmp
+    zip -j $target_folder/$2 $target_folder/$2_tmp/hive
     rm -r $target_folder/$2_tmp
 }
 
@@ -26,7 +27,10 @@ BuildWindows() {
     mkdir -p $target_folder/$2_tmp/bin
     cp ./target/$1/release/hive.exe $target_folder/$2_tmp/bin
     cp ./hive.bat $target_folder/$2_tmp
-    zip -r $target_folder/$2 $target_folder/$2_tmp/*
+    cd $target_folder/$2_tmp/
+    zip -r ./$2.zip *
+    cd - > /dev/null
+    mv $target_folder/$2_tmp/$2.zip $target_folder/$2.zip
     rm -r $target_folder/$2_tmp
 }
 
